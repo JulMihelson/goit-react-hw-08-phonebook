16f14ddf0e81f1d27337783055a65d2ce82c5b6e
jul.mihelson@gmail.com <125132083+JulMihelson@users.noreply.github.com>
2023-06-20 17:06:37 +0300

    Update UserMenu.jsx

--- src/components/UserMenu.jsx
+++ src/components/UserMenu.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const userMenu = () => {
   return (
     <div>
-      <p>{mango@mail.com}</p>
+      <p>mango@mail.com</p>
       <button>Logout</button>
     </div>
   );
